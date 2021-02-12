03a24756b56fb4c87f91bf311836bbc835c7dcdc
JPCHATEAU01 <72606011+JPCHATEAU01@users.noreply.github.com>
2021-02-12 17:45:13 +0100

    Create Category.java

---
+++ src/main/java/com/example/demo/entities/Category.java
@@ -0,0 +1,29 @@
+package com.example.demo.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //A changer
+    public void sortLicence(){
+        
+    }
+}
