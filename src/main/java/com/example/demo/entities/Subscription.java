4b298226ea410ea9fbb76155244c7d27942672fe
JPCHATEAU01 <72606011+JPCHATEAU01@users.noreply.github.com>
2021-03-04 19:22:04 +0100

    add Subscription

--- src/main/java/com/example/demo/entities/Subscription.java
+++ src/main/java/com/example/demo/entities/Subscription.java
@@ -12,5 +12,38 @@ public class Subscription {
     private Integer id;
     private String name;
     private float price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
     private String text;
 }
