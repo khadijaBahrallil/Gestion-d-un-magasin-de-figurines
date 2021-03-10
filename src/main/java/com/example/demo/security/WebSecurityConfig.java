fdcfcdf076020eabbb334beb7b3f9235502b72f2
khadijaBahrallil <khadija9932.kb@gmail.com>
2021-03-10 14:05:13 -0500

    Fix Logout

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -28,8 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new MyAuthenticationSuccessHandler();
     }
 
-    @Autowired
-    private LogoutSuccessHandler myLogoutSuccessHandler;
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -93,7 +92,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessHandler(myLogoutSuccessHandler)
+
 
                 .logoutSuccessUrl("/indexlogout")
 
